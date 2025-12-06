088ad6466684ea1529e36f7061bdcfa367aaf648
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-12-06 19:22:33 +0100

    agregar clases a bibliotecas

---
+++ HexagonalDemo.Api/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HexagonalDemo.Api.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
