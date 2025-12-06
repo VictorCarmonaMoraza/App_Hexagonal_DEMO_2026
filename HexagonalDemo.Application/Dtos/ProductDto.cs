6809672b29b44929ea910a7befc82de7fc791582
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-12-06 19:37:58 +0100

    Rellenamos la clae ProductDto

--- HexagonalDemo.Application/Dtos/ProductDto.cs
+++ HexagonalDemo.Application/Dtos/ProductDto.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HexagonalDemo.Application.Dtos
+﻿namespace HexagonalDemo.Application.Dtos
 {
-    internal class ProductDto
+    public class ProductDto
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
--- HexagonalDemo.Application/Ports/IProductRepository.cs
+++ HexagonalDemo.Application/Ports/IProductRepository.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HexagonalDemo.Domain.Entities;
 
 namespace HexagonalDemo.Application.Ports
 {
     public interface IProductRepository
     {
+        Task<Product?> GetByIdAsync(int id);
     }
 }
