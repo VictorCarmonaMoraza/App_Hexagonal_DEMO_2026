78facc89be60408e669d032f8cda8e3d39c6ec0f
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-12-06 19:31:06 +0100

    Creacion de la entidad Product en el Domain

--- HexagonalDemo.Domain/Entities/Product.cs
+++ HexagonalDemo.Domain/Entities/Product.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HexagonalDemo.Domain.Entities
+﻿namespace HexagonalDemo.Domain.Entities
 {
-    internal class Product
+    public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
