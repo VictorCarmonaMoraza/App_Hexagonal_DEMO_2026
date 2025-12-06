1efa0dcbac4e7b2eef2b01b0ad0f7806a7fc5fc7
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-12-06 19:42:17 +0100

    Creacion de caso de uso

--- HexagonalDemo.Application/UseCases/GetProductUseCase.cs
+++ HexagonalDemo.Application/UseCases/GetProductUseCase.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HexagonalDemo.Application.Dtos;
+using HexagonalDemo.Application.Ports;
 
 namespace HexagonalDemo.Application.UseCases
 {
-    internal class GetProductUseCase
+    public class GetProductUseCase
     {
+        private readonly IProductRepository _repo;
+
+        public GetProductUseCase(IProductRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<ProductDto?> Execute(int id)
+        {
+            var product = await _repo.GetByIdAsync(id);
+
+            if (product == null) return null;
+
+            return new ProductDto
+            {
+                Id = product.Id,
+                Name = product.Name
+            };
+        }
     }
 }
