e6995a069a92716cf4b132b7110642c4e1262210
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-12-06 19:43:27 +0100

    Agregar datos de prueba o clase fake

--- HexagonalDemo.Infrastructure/Data/FakeDb.cs
+++ HexagonalDemo.Infrastructure/Data/FakeDb.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HexagonalDemo.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace HexagonalDemo.Infrastructure.Data
 {
-    internal class FakeDb
+    public static class FakeDb
     {
+        public static List<Product> Products = new()
+        {
+            new Product { Id = 1, Name = "Laptop" },
+            new Product { Id = 2, Name = "Teclado" }
+        };
     }
 }
